c646acbc0bd4032abb2886ea49fa22439a25887a
Doohan Aditya Saputro <doohanaditya@yahoo.co.id>
2022-07-16 15:06:12 +0700

    chore (nuxt-config): add router config

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,10 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  router: {
+    base: '/doohanas.github.io/'
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'doohanas.github.io',
